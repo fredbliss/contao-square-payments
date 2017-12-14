138de582d619bc6da4bc750e351b6a52139f8457
fbliss <fred.bliss@intelligentspark.com>
2017-12-14 14:23:10 -0500

    added generate function

--- library/IntelligentSpark/Module/Square.php
+++ library/IntelligentSpark/Module/Square.php
@@ -22,6 +22,29 @@ class Square extends \Module {
     protected $strTemplate = 'mod_square';
 
 
+    /**
+     * Display a wildcard in the back end
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE')
+        {
+            /** @var \BackendTemplate|object $objTemplate */
+            $objTemplate = new \BackendTemplate('be_wildcard');
+
+            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['square'][0]) . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+
+            return $objTemplate->parse();
+        }
+
+        return parent::generate();
+    }
 
     /**
      * Generate the module
